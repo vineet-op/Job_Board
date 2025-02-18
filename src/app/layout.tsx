5c09c6f78269cf6b0b4ea0c54fd0318e0b1be55b
Vineet Jadhav <vineet290403@gmail.com>
2025-02-18 23:48:49 +0530

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
         <Toaster />
       </body>
     </html>
